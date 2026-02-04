7598e823ab9f51a3dc12e375d7cc01fb43a3ea67
flyingking888 <jeremyfeiwang@outlook.com>
2026-02-04 16:33:06 -0500

    2-4-2026

--- Flagler Student/HomeViewController.swift
+++ Flagler Student/HomeViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+class HomeViewController: UIViewController {
+    
+    //Put your IBOutlets before viewDidLoad
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var majorTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func submitButton(_ sender: UIButton) {
+        //Get user inputs (optionals)
+        let name = nameTextField.text ?? ""
+        let major = majorTextField.text ?? "No Major"
+        //Shorter form but a little dangerous
+        
+        //Forced unwrapping, which could cause errors.
+        //let alternativeName = nameTextField.text!
+        
+        //Enhancements - could validate them?
+        
+        //Call the alert object and do popup.
+        
+        let message = "Hello world, \(name)! Thank you for your interest in \(major) major! :)"
+        
+        let alert = UIAlertController(
+            title: "Greetings",
+            message: message,
+            preferredStyle: .alert)
+        
+        alert.addAction(
+            UIAlertAction(title: "OK", style: .default)
+        )
+        
+        present(alert, animated: true)
+    
+        
+    }
+    
 }
 
